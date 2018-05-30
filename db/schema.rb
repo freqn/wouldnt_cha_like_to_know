9cf25501b4a5fc3436507f3ce5be08d4ce12f907
freqn <ceyler@gmail.com>
2018-05-29 22:04:59 -0400

    add name to users and strong params for registrations

--- db/schema.rb
+++ db/schema.rb
@@ -13,78 +13,7 @@
 ActiveRecord::Schema.define(version: 2018_05_30_010854) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "fuzzystrmatch"
   enable_extension "plpgsql"
-  enable_extension "postgis"
-  enable_extension "postgis_tiger_geocoder"
-  enable_extension "postgis_topology"
-  enable_extension "tablefunc"
-  enable_extension "uuid-ossp"
-
-  create_table "activation", primary_key: "activation_uid", id: :uuid, default: nil, force: :cascade do |t|
-    t.uuid "user_uid", null: false
-    t.boolean "is_active", default: true, null: false
-    t.datetime "created_date", default: -> { "clock_timestamp()" }, null: false
-    t.datetime "updated_date"
-    t.uuid "created_uid", default: -> { "get_user()" }
-    t.uuid "updated_uid"
-    t.string "language", limit: 2, default: "en"
-    t.string "dialect", limit: 2, default: "us"
-  end
-
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "layer", primary_key: ["topology_id", "layer_id"], force: :cascade do |t|
-    t.integer "topology_id", null: false
-    t.integer "layer_id", null: false
-    t.string "schema_name", null: false
-    t.string "table_name", null: false
-    t.string "feature_column", null: false
-    t.integer "feature_type", null: false
-    t.integer "level", default: 0, null: false
-    t.integer "child_id"
-    t.index ["schema_name", "table_name", "feature_column"], name: "layer_schema_name_table_name_feature_column_key", unique: true
-  end
-
-  create_table "schema_info", id: false, force: :cascade do |t|
-    t.integer "version", default: 0, null: false
-  end
-
-  create_table "schema_updates", id: false, force: :cascade do |t|
-    t.integer "version"
-    t.datetime "updated_date"
-  end
-
-  create_table "spatial_ref_sys", primary_key: "srid", id: :integer, default: nil, force: :cascade do |t|
-    t.string "auth_name", limit: 256
-    t.integer "auth_srid"
-    t.string "srtext", limit: 2048
-    t.string "proj4text", limit: 2048
-  end
-
-  create_table "tmp_quest_map", id: false, force: :cascade do |t|
-    t.uuid "question_uid"
-    t.text "question_code"
-  end
-
-  create_table "topology", id: :serial, force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "srid", null: false
-    t.float "precision", null: false
-    t.boolean "hasz", default: false, null: false
-    t.index ["name"], name: "topology_name_key", unique: true
-  end
-
-  create_table "tweeets", force: :cascade do |t|
-    t.text "tweeet"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -99,13 +28,9 @@ ActiveRecord::Schema.define(version: 2018_05_30_010854) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "v_event_type_uid", id: false, force: :cascade do |t|
-    t.uuid "event_type_uid"
-  end
-
-  add_foreign_key "layer", "topology", name: "layer_topology_id_fkey"
 end
